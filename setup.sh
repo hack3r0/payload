c555de9cbf2c5cad8d7d65e8844631be49358510
H4ck3r <78647234+hack3r0@users.noreply.github.com>
2022-11-05 20:06:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,6 @@ echo -e "\e[1;34m[*] \e[32minstall packages....\e[0m";
 apt update && apt upgrade -y
 pkg install ruby python python2 -y
 pkg install toilet -y
-pkg install openjdk-17 -y
 pip3 install gem
 gem install lolcat
 pkg install wget curl -y
